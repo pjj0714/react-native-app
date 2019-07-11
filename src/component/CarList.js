5041d021ac9775aebce3b41b553ba040e9e90fd2
pjj0714 <pjj1587@gmail.com>
2019-07-11 20:36:58 +0900

    Complete vehicle details page layout

--- src/component/CarList.js
+++ src/component/CarList.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Text, View, TextInput, ScrollView } from 'react-native';
+import { SearchBar, colors } from 'react-native-elements';
 import CarListDetail from './CarListDetail';
+import Icon from 'react-native-vector-icons/AntDesign';
 
 class CarList extends React.Component {
+  static navigationOptions = {
+    header: null
+  };
+
   render() {
     const { navigation } = this.props;
     const { data } = navigation.state.params;
@@ -17,7 +23,40 @@ class CarList extends React.Component {
         />
       );
     });
-    return <View>{renderList}</View>;
+    return (
+      // <View style={{ flex: 1 }}>
+      //   <Text style={{ backgroundColor: 'red', flex: 0.1 }} />
+      //   <Text style={{ backgroundColor: 'blue', flex: 0.8 }} />
+      //   <Text style={{ backgroundColor: 'pink', flex: 0.1 }} />
+      // </View>
+      <View style={{ flex: 1 }}>
+        <SearchBar
+          style={{ flex: 0.1 }}
+          containerStyle={{
+            marginTop: 24,
+            backgroundColor: '#ffffff',
+            borderBottomWidth: 1,
+            borderBottomColor: 'black'
+          }}
+          inputContainerStyle={{ backgroundColor: '#ffffff' }}
+          icon={{
+            type: 'font-awesome',
+            name: 'search',
+            style: { color: '#00bc45' }
+          }}
+          // TextInput style={{ marginTop: 24, fontSize: 24, height: 48 }}
+          placeholder="차량 정보를 검색하세요"
+          placeholderTextColor="gray"
+        />
+        <ScrollView style={{ flex: 0.8 }} showsVerticalScrollIndicator={false}>
+          {renderList}
+        </ScrollView>
+        <Text style={{ flex: 0.1, backgroundColor: '#00bc45', marginTop: 16 }}>
+          Blank
+        </Text>
+      </View>
+    );
   }
 }
+
 export default CarList;
--- src/component/CarListDetail.js
+++ src/component/CarListDetail.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign';
 
 const CarListDetail = ({ capacity, favorite, description, licenseNumber }) => {
   return (
-    <View>
-      <Text>{capacity}</Text>
-      <Text>{favorite}</Text>
-      <Text>{description}</Text>
-      <Text>{licenseNumber}</Text>
+    <View
+      style={{
+        borderLeftWidth: 1,
+        borderRightWidth: 1,
+        borderBottomWidth: 2,
+        borderRadius: 5,
+        borderColor: 'balck',
+        margin: 8
+      }}>
+      <Text style={{ fontSize: 12, margin: 8 }}>{description}</Text>
+      <Text style={{ fontSize: 20, margin: 8 }}>{licenseNumber}</Text>
+      <View style={{ flex: 1, flexDirection: 'row', margin: 8 }}>
+        <Icon name="star" />
+        <Text style={{ fontSize: 14, marginLeft: 8 }}>
+          적재용량 : {`${capacity / 1000}t`}
+        </Text>
+      </View>
     </View>
   );
 };
