e9c72de20329c21a3be50e0eb11d6d319fcb89b6
pjj0714 <pjj1587@gmail.com>
2019-07-14 16:33:42 +0900

    Add favorite reducer and vehiclesDataReducer

--- src/Reducers/rootReducer.js
+++ src/Reducers/rootReducer.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import login from './loginReducer';
+import vehiclesData from './vehiclesDataReducer';
+import favorite from './favoriteReducer';
 
 const rootReducer = combineReducers({
-  login
+  login,
+  vehiclesData,
+  favorite
 });
 
 export default rootReducer;
